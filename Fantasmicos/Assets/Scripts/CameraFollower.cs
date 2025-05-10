1f65c22362ab1a300060e752ce0c8370ebf6e51d
Francisco Matas Moreno <pacomm@uma.es>
2025-05-10 13:20:05 +0200

    camera arreglada

--- Fantasmicos/Assets/Scripts/CameraFollower.cs
+++ Fantasmicos/Assets/Scripts/CameraFollower.cs
@@ -6,12 +6,12 @@ public class CameraFollower : MonoBehaviour
     public Vector3 offset = new Vector3(0f, 5f, -10f); // posición relativa a él
     public float smoothSpeed = 0.125f; // suavizado
 
-    void LateUpdate()
+    void Update()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-        transform.LookAt(target); // opcional: que la cámara mire al fantasma
+        transform.LookAt(target);
     }
 }
