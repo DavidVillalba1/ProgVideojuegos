e707603686a2e836c1cd79bba970af9b602fef28
Francisco Matas Moreno <pacomm@uma.es>
2025-05-10 14:00:26 +0200

    sala principal

--- Fantasmicos/Assets/Scripts/GhostController.cs
+++ Fantasmicos/Assets/Scripts/GhostController.cs
@@ -7,8 +7,12 @@ public class GhostController : MonoBehaviour
     public float velocidad = 5.0f;
     private Rigidbody rb;
 
+    public Transform playerStart;
+
     void Start()
     {
+        // Set the initial position of the ghost to the player's start position
+        transform.position = playerStart.position;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -19,12 +23,11 @@ public class GhostController : MonoBehaviour
 
         Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-        if(movimiento.magnitude > 0.1f)
+        if (movimiento.magnitude > 0.1f)
         {
-
-            Quaternion toRotation = Quaternion.LookRotation(movimiento, Vector3.up);
+            Quaternion toRotation = Quaternion.LookRotation(-movimiento, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
         }
-        rb.MovePosition(transform.position + movimiento.normalized * velocidad * Time.deltaTime);
+        rb.MovePosition(transform.position + movimiento.normalized * velocidad * Time.deltaTime * -1);
     }
 }
